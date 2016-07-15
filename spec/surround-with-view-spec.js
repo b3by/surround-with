190497949bbfce6f19a66b9dc129390939569769
b3by <b3by.in.th3.sky@gmail.com>
2016-07-15 10:55:31 +0100

    first push

---
+++ spec/surround-with-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SurroundWithView from '../lib/surround-with-view';
+
+describe('SurroundWithView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
